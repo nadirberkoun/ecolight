e1c7e02fc76b1a8319500d8b8957504b1c81a937
nadirberkoun <nberkoun@orchestra-premaman.com>
2017-03-27 17:56:29 +0200

    block-supression-en-partie

--- Theme-blocks/calenrier.php
+++ Theme-blocks/calenrier.php
@@ -134,13 +134,13 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="index.html"><img src="assets/img/logo30.png" alt=""> BLOCKS Dashboard</a>
+          <a class="navbar-brand" href="index.php"><img src="assets/img/logo30.png" alt=""> BLOCKS Dashboard</a>
         </div>
           <div class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
-              <li><a href="index.html"><i class="icon-home icon-white"></i> Home</a></li>
-              <li class="active"><a href="calendar.html"><i class="icon-calendar icon-white"></i> Calendar</a></li>
-              <li><a href="tables.html"><i class="icon-th icon-white"></i> Tables</a></li>
+              <li><a href="index.php"><i class="icon-home icon-white"></i> Home</a></li>
+              <li class="active"><a href="calendrier.php"><i class="icon-calendar icon-white"></i> Calendar</a></li>
+              <li><a href="tables.php"><i class="icon-th icon-white"></i> Tables</a></li>
 
             </ul>
           </div><!--/.nav-collapse -->
